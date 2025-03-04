c6abc8bc3ed7a8ff29664796212f9f18a4d93eeb
Dhrutii <thakkardhruti22@gmail.com>
2025-03-04 23:55:55 +0530

    add: passport.js file

---
+++ server/utils/passport.js
@@ -0,0 +1,18 @@
+import passport from "passport";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:3000/api/google/callback",
+      passReqToCallback: true,
+    },
+    function (accessToken, refreshToken, profile, cb) {
+      User.findOrCreate({ googleId: profile.id }, function (err, user) {
+        return cb(err, user);
+      });
+    }
+  )
+);
