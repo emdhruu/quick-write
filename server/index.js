35bffa789522fd69dad8fab75f7f18a7700fa50b
Dhrutii <thakkardhruti22@gmail.com>
2025-03-04 21:22:38 +0530

    common imports

---
+++ server/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import session from "express-session";
+import { mainRouter } from "./routes/index.js";
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cookieParser());
+
+app.use("/api", mainRouter);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
