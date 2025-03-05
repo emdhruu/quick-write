1946a277b1e3d7e91abfe4e4b791e3b3bad57413
Dhrutii <thakkardhruti22@gmail.com>
2025-03-05 14:28:01 +0530

    routes /document/save

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import authRoutes from "./auth.routes.js";
+import saveRoutes from "./save.routes.js";
 
 const mainRouter = Router();
 
 mainRouter.get("/auth", authRoutes);
+mainRouter.post("/document", saveRoutes);
 
 export { mainRouter };
